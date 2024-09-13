4c1e733381168e1fed20111a1fc9fdcad88a6a4f
Hryniuk_Taras <gritaras484@gmail.com>
2024-09-13 09:42:40 +0300

    make build

---
+++ index.574e72b8.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),l=e.clientWidth,c=e.clientHeight,i=t.clientWidth,n=t.clientHeight;t.style.position="absolute",t.style.top="".concat((c-n)/2,"px"),t.style.left="".concat((l-i)/2,"px"),e.append(t);
+//# sourceMappingURL=index.574e72b8.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallWidth = wall.clientWidth;
+const wallHeight = wall.clientHeight;
+const spiderWidth = spider.clientWidth;
+const spiderHeight = spider.clientHeight;
+const spiderLeft = (wallWidth - spiderWidth) / 2;
+const spiderTop = (wallHeight - spiderHeight) / 2;
+spider.style.position = "absolute";
+spider.style.top = `${spiderTop}px`;
+spider.style.left = `${spiderLeft}px`;
+wall.append(spider);
+
+//# sourceMappingURL=index.f75de5e1.js.map
